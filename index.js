9f20207f6daf2d4c81f5a29dfec2b1b9c08a6183
wilop <slayfel_ii@hotmail.com>
2023-04-03 16:08:40 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1>Hello World with Github</h1>');
+});
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
