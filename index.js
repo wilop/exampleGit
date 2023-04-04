d1eb762499d4150b87d3cad4344dc41b0f2ffcf7
Wilberth Antonio López Aguilar <slayfel_ii@hotmail.com>
2023-04-03 22:55:43 -0600

    Update index.js
    an update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).send('<h1>Hello World with Github</h1>');
+    res.status(200).send('<h1>Hello World with Github to Azure</h1>');
 });
 
 
